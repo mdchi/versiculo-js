bdef91d450f743a5b963e42742052af73c0c202a
Mariano Damian <51171480+marianodamianch@users.noreply.github.com>
2020-02-15 13:57:20 -0300

    Update randomize.js

--- randomize.js
+++ randomize.js
@@ -4,16 +4,16 @@ var imgRnd = new Array();
 
 //asignamos una ruta de imagen a cada celda del array
 //assign an image path to each cell in the array
-imgRnd[0]="./img/1.gif";
-imgRnd[1]="./img/2.gif";
-imgRnd[2]="./img/3.gif";
-imgRnd[3]="./img/4.gif";
-imgRnd[4]="./img/5.gif";
-imgRnd[5]="./img/6.gif";
-imgRnd[6]="./img/7.gif";
-imgRnd[7]="./img/8.gif";
-imgRnd[8]="./img/9.gif";
-imgRnd[9]="./img/10.gif";
+imgRnd[0]="https://marianodamianch.github.io/versiculo/jpg/001.jpg";
+imgRnd[1]="https://marianodamianch.github.io/versiculo/jpg/002.jpg";
+imgRnd[2]="https://marianodamianch.github.io/versiculo/jpg/003.jpg";
+imgRnd[3]="https://marianodamianch.github.io/versiculo/jpg/004.jpg";
+imgRnd[4]="https://marianodamianch.github.io/versiculo/jpg/005.jpg";
+imgRnd[5]="https://marianodamianch.github.io/versiculo/jpg/006.jpg";
+imgRnd[6]="https://marianodamianch.github.io/versiculo/jpg/007.jpg";
+imgRnd[7]="https://marianodamianch.github.io/versiculo/jpg/008.jpg";
+imgRnd[8]="https://marianodamianch.github.io/versiculo/jpg/009.jpg";
+imgRnd[9]="https://marianodamianch.github.io/versiculo/jpg/010.jpg";
 
 //imgRnd[0]="../img/1.gif";
 //imgRnd[1]="../img/2.gif";
